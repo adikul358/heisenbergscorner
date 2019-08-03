211658060439a0d09777099ff96fd919552db0b2
Aditya Kulshrestha <adi.kul358@gmail.com>
2019-08-03 23:52:38 +0530

    remake init

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -9,10 +10,14 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/submit-answer/user-data', function(request, response){
+    userData.test(request.body);
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
