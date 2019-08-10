6709899e6484f4b882324a7f61f6228530194f30
Aditya Kulshrestha <adi.kul358@gmail.com>
2019-08-10 23:35:00 +0530

    db

--- models/db.js
+++ models/db.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/hc-main', { useNewUrlParser: true }, (err) => {
-    if (!err) { console.log('MongoDB Connection Succeeded.') }
-    else { console.log('Error in DB connection : ' + err) }
-});
-// mongoose.connect('mongodb://aditya:aditya358@153.92.5.101/hc-main', { useNewUrlParser: true }, (err) => {
+// mongoose.connect('mongodb://localhost:27017/hc-main', { useNewUrlParser: true }, (err) => {
 //     if (!err) { console.log('MongoDB Connection Succeeded.') }
 //     else { console.log('Error in DB connection : ' + err) }
 // });
+mongoose.connect('mongodb://admin:adminsnsnsteam@153.92.5.101/hc-main?authSource=admin', { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log('MongoDB Connection Succeeded.') }
+    else { console.log('Error in DB connection : ' + err) }
+});
 
 require('./question.model');
 require('./answer.model');
